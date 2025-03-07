fd7ecbd397fcd99d303a896ee8934ab487ee7f98
zxlee0114 <napoleon.lee0114@gmail.com>
2025-03-07 13:39:00 +0800

    feat: 為產品頁面加上 container

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -41,79 +41,81 @@ function ProductsPage() {
   };
   return (
     <>
-      <table className="table align-middle">
-        <thead>
-          <tr>
-            <th>圖片</th>
-            <th>商品名稱</th>
-            <th>價格</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td style={{ width: "200px" }}>
-                <img
-                  className="img-fluid"
-                  src={product.imageUrl}
-                  alt={product.title}
-                />
-              </td>
-              <td>{product.title}</td>
-              <td>
-                <del className="h6">原價 {product.origin_price} 元</del>
-                <div className="h5">特價 {product.price}元</div>
-              </td>
-              <td>
-                <div className="btn-group btn-group-sm">
-                  <Link
-                    to={`/products/${product.id}`}
-                    className="btn btn-outline-secondary"
-                  >
-                    查看更多
-                  </Link>
-                  <button
-                    onClick={() => {
-                      addCartItem(product.id, 1);
-                    }}
-                    type="button"
-                    className="btn btn-outline-danger"
-                    style={{
-                      cursor: isBtnLoading ? "not-allowed" : "pointer",
-                      opacity: isBtnLoading ? 0.7 : 1,
-                      transition: "all 0.3s",
-                    }}
-                  >
-                    加到購物車{" "}
-                    {isBtnLoading && (
-                      <BeatLoader size={8} color="#ffffff" margin={2} />
-                    )}
-                  </button>
-                </div>
-              </td>
+      <div className="container">
+        <table className="table align-middle">
+          <thead>
+            <tr>
+              <th>圖片</th>
+              <th>商品名稱</th>
+              <th>價格</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td style={{ width: "200px" }}>
+                  <img
+                    className="img-fluid"
+                    src={product.imageUrl}
+                    alt={product.title}
+                  />
+                </td>
+                <td>{product.title}</td>
+                <td>
+                  <del className="h6">原價 {product.origin_price} 元</del>
+                  <div className="h5">特價 {product.price}元</div>
+                </td>
+                <td>
+                  <div className="btn-group btn-group-sm">
+                    <Link
+                      to={`/products/${product.id}`}
+                      className="btn btn-outline-secondary"
+                    >
+                      查看更多
+                    </Link>
+                    <button
+                      onClick={() => {
+                        addCartItem(product.id, 1);
+                      }}
+                      type="button"
+                      className="btn btn-outline-danger"
+                      style={{
+                        cursor: isBtnLoading ? "not-allowed" : "pointer",
+                        opacity: isBtnLoading ? 0.7 : 1,
+                        transition: "all 0.3s",
+                      }}
+                    >
+                      加到購物車{" "}
+                      {isBtnLoading && (
+                        <BeatLoader size={8} color="#ffffff" margin={2} />
+                      )}
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
-      {/* loading */}
-      {isScreenLoading && (
-        <div
-          className="loading-container"
-          style={{
-            position: "fixed",
-            inset: 0,
-            backgroundColor: "rgba(255,255,255,0.3)",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            zIndex: 9999,
-          }}
-        >
-          <ClipLoader color="#36D7B7" loading={true} size={50} />
-        </div>
-      )}
+        {/* loading */}
+        {isScreenLoading && (
+          <div
+            className="loading-container"
+            style={{
+              position: "fixed",
+              inset: 0,
+              backgroundColor: "rgba(255,255,255,0.3)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: 9999,
+            }}
+          >
+            <ClipLoader color="#36D7B7" loading={true} size={50} />
+          </div>
+        )}
+      </div>
     </>
   );
 }
